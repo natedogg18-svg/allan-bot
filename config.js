a59ad0d3938ebf407cfa07d04fbe71650450aabb
Nate <natedogg18@gmail.com>
2026-06-16 04:04:53 -0600

    use env token

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  token: "MTUxNDkxOTY5NDIwMzYxNzQ1MA.GDkCJd.Y8Sdi4yEYyR_da-3UQQMHP2KFAMAeXBNzRWbBs",
+  token: process.env.DISCORD_TOKEN,
   prefix: "!",
   afkTimeoutMinutes: 60,      // 45min alone + 15min after Allan joins
   aloneTimeoutMinutes: 45,    // how long someone can be alone before Allan joins
